155c0f9fd98b9c5c5acb227c7bc0ba8c2a7b2fcb
cchoirat <cchoirat@gmail.com>
2014-11-03 16:45:22 -0500

    Package skeleton

---
+++ R/myfunction.R
@@ -0,0 +1,23 @@
+#' Adds 3.
+#' 
+#' @param x A numeric quantity.
+#' @return The sum of \code{x} 3.
+#' @examples
+#' tortilla(5)
+#' tortilla(1:5)
+tortilla <- function(x) {
+  return(x + 3)
+}
+
+
+#' Returns a list of two elements.
+#' 
+#' @param x Any R object.
+#' @param y Any R object.
+#' @return A list with first element \code{x} and second element \code{y}.
+#' @examples
+#' polenta(10, 1)
+#' polenta("10", 1:5)
+polenta <- function(x, y) {
+  return(list(a = x, b = y))
+}
